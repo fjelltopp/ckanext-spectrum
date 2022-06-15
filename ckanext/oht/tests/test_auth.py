f7499af6b467786255544c5a762ebac6ae958654
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-06-15 11:58:36 +0100

    OHT-32 Setup with fixtures

--- ckanext/oht/tests/test_auth.py
+++ ckanext/oht/tests/test_auth.py
@@ -5,107 +5,122 @@ from ckanext.oht.tests import get_context
 from ckan.plugins import toolkit
 
 
+@pytest.fixture
+def users():
+    return [
+        factories.User(),
+        factories.User()
+    ]
+
+
+@pytest.fixture
+def organisation(request, users):
+
+    return factories.Organization(
+        users=[
+            {'name': users[0]['name'], 'capacity': 'editor'},
+            {'name': users[1]['name'], 'capacity': 'editor'}
+        ]
+    )
+
+
+@pytest.fixture
+def datasets(organisation, users):
+    return [
+        factories.Dataset(
+            owner_org=organisation['id'],
+            type='oht',
+            private=True,
+            user=users[0]
+        ),
+        factories.Dataset(
+            owner_org=organisation['id'],
+            type='oht',
+            private=True,
+            user=users[1]
+        ),
+        factories.Dataset(
+            owner_org=organisation['id'],
+            type='oht',
+            private=False,
+            user=users[1]
+        )
+    ]
+
+
 @pytest.mark.usefixtures("clean_db")
 class TestAuth():
 
-    def setup(self):
-        self.user_1 = factories.User()
-        self.user_2 = factories.User()
-        self.org = factories.Organization(
-            users=[
-                {'name': self.user_1['name'], 'capacity': 'editor'},
-                {'name': self.user_2['name'], 'capacity': 'editor'}
-            ]
-        )
-        self.dataset_1 = factories.Dataset(
-            owner_org=self.org['id'],
-            type='oht',
-            private=True,
-            user=self.user_1
-        )
-        self.dataset_2 = factories.Dataset(
-            owner_org=self.org['id'],
-            type='oht',
-            private=True,
-            user=self.user_2
-        )
-        self.dataset_3 = factories.Dataset(
-            owner_org=self.org['id'],
-            type='oht',
-            private=False,
-            user=self.user_2
-        )
-
-    def test_users_cant_see_other_users_private_datasets(self):
+    def test_users_cant_see_private_datasets(self, users, datasets):
 
         with pytest.raises(toolkit.NotAuthorized):
             call_auth(
                 'package_show',
-                get_context(self.user_1),
-                id=self.dataset_2['id']
+                get_context(users[0]),
+                id=datasets[1]['id']
             )
 
-    def test_users_cant_edit_other_users_private_datasets(self):
+    def test_users_cant_edit_private_datasets(self, users, datasets):
 
         with pytest.raises(toolkit.NotAuthorized):
             call_auth(
                 'package_update',
-                get_context(self.user_1),
-                id=self.dataset_2['id']
+                get_context(users[0]),
+                id=datasets[1]['id']
             )
 
-    def test_users_can_edit_own_datasets(self):
+    def test_users_can_edit_own_datasets(self, users, datasets):
         assert call_auth(
             'package_update',
-            get_context(self.user_1),
-            id=self.dataset_1['id']
+            get_context(users[0]),
+            id=datasets[0]['id']
         )
 
-    def test_users_can_see_own_datasets(self):
+    def test_users_can_see_own_datasets(self, users, datasets):
         assert call_auth(
             'package_show',
-            get_context(self.user_1),
-            id=self.dataset_1['id'],
+            get_context(users[0]),
+            id=datasets[0]['id']
         )
 
-    def test_collaborators_can_see_datasets(self):
+    def test_collaborators_can_see_datasets(self, users, datasets):
         call_action(
             'package_collaborator_create',
-            id=self.dataset_1['id'],
-            user_id=self.user_2['id'],
+            id=datasets[0]['id'],
+            user_id=users[1]['id'],
             capacity='member'
         )
         assert call_auth(
             'package_show',
-            get_context(self.user_2),
-            id=self.dataset_1['id'],
+            get_context(users[1]),
+            id=datasets[0]['id'],
         )
 
-    def test_collaborators_can_edit_datasets(self):
+    def test_collaborators_can_edit_datasets(self, users, datasets):
         call_action(
             'package_collaborator_create',
-            id=self.dataset_2['id'],
-            user_id=self.user_1['id'],
+            id=datasets[0]['id'],
+            user_id=users[1]['id'],
             capacity='editor'
         )
         assert call_auth(
             'package_update',
-            get_context(self.user_1),
-            id=self.dataset_2['id'],
+            get_context(users[1]),
+            id=datasets[0]['id'],
         )
 
-    def test_users_can_see_public_datasets(self):
+    def test_users_can_see_public_datasets(self, users, datasets):
         assert call_auth(
             'package_show',
-            get_context(self.user_1),
-            id=self.dataset_3['id'],
+            get_context(users[0]),
+            id=datasets[2]['id'],
         )
 
-    def test_users_require_permission_to_edit_public_datasets(self):
+    def test_users_cant_edit_public_datasets(self, users, datasets):
 
         with pytest.raises(toolkit.NotAuthorized):
             call_auth(
                 'package_update',
-                get_context(self.user_1),
-                id=self.dataset_3['id']
+                get_context(users[0]),
+                id=datasets[2]['id']
             )
