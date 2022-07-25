25e0ce6b3e7c8e22705d93d14069fa541a178638
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-07-25 13:31:17 +0100

    OHT-63 Chained action to autogenerate passwords

---
+++ ckanext/oht/actions.py
@@ -0,0 +1,14 @@
+import ckan.plugins.toolkit as toolkit
+import secrets
+
+
+@toolkit.chained_action
+def user_create(next_action, context, data_dict):
+    """
+    Autogenerates a password (if password is not provided).
+    """
+
+    if not data_dict.get('password'):
+        data_dict['password'] = secrets.token_urlsafe(32)
+
+    return next_action(context, data_dict)
