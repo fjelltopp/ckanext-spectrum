957eb115bb46416156f49b4e291198d5943e4dbb
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-08-10 14:59:35 +0100

    SPC-79 Working proof of concept

--- ckanext/spectrum/actions.py
+++ ckanext/spectrum/actions.py
@@ -1,5 +1,9 @@
 import ckan.plugins.toolkit as toolkit
+from ckanext.blob_storage.blueprints import download
+from werkzeug.datastructures import FileStorage
+from io import BytesIO
 import secrets
+import requests
 
 
 @toolkit.chained_action
@@ -15,8 +19,9 @@ def user_create(next_action, context, data_dict):
 
 
 def dataset_duplicate(context, data_dict):
-    dataset_id = toolkit.get_or_bust(data_dict, 'id')
-    dataset = toolkit.get_action('package_show')(context, {'id': dataset_id})
+    dataset_id_or_name = toolkit.get_or_bust(data_dict, 'id')
+    dataset = toolkit.get_action('package_show')(context, {'id': dataset_id_or_name})
+    dataset_id = dataset['id']
     resources = dataset.pop('resources', [])
 
     del dataset['id']
@@ -28,7 +33,18 @@ def dataset_duplicate(context, data_dict):
     new_dataset = toolkit.get_action('package_create')(context, dataset)
 
     for resource in resources:
+        filename = resource.get('url', "").split('/')[-1]
+        blob_storage_response = download(dataset_id, resource['id'], filename)
+        file_response = requests.get(blob_storage_response.headers.get('Location'), stream=True)
+        file_object = FileStorage(BytesIO(file_response.content), filename, 'upload')
+
+        del resource['size']
+        del resource['sha256']
+        del resource['lfs_prefix']
+        del resource['url']
+
         resource['package_id'] = new_dataset['id']
+        resource['upload'] = file_object
         toolkit.get_action('resource_create')(context, resource)
 
     return toolkit.get_action('package_show')(context, {'id': new_dataset['id']})
