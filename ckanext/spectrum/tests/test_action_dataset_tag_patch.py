889a10d779f9305cc7f66b203571b1a5e07649aa
Kamil Forenc <kamil.forenc@gmail.com>
2022-12-05 15:21:14 +0100

    SPC-96 Fix failing test

--- ckanext/spectrum/tests/test_action_dataset_tag_patch.py
+++ ckanext/spectrum/tests/test_action_dataset_tag_patch.py
@@ -1,13 +1,11 @@
 import pytest
 
-from ckan import logic
-from ckan.tests.helpers import call_action
-from ckanext.spectrum.actions import user_create, _record_dataset_duplication
 import ckan.tests.factories as factories
-from ckanext.spectrum.tests import get_context
 from ckan.plugins import toolkit
+from ckan.tests.helpers import call_action
 
 
+@pytest.mark.ckan_config('ckan.plugins', "spectrum scheming_datasets")
 @pytest.mark.usefixtures('clean_db', 'with_plugins')
 class TestDatasetTagPatch():
 
