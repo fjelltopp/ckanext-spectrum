9243c9ca2031b33b4409902316ce4e615dab52a1
Jonathan S Berry <jonathan.s.berry@gmail.com>
2023-05-17 14:59:26 +0100

    SPC-116 Temporary fix for user identification

--- ckanext/spectrum/authn.py
+++ ckanext/spectrum/authn.py
@@ -1,5 +1,5 @@
 import ckan.model as model
-import ckan.plugins.toolkit as toolkit
+from flask import current_app
 
 
 def substitute_user(substitute_user_id):
@@ -15,4 +15,7 @@ def substitute_user(substitute_user_id):
             }
         }, 400
 
-    toolkit.login_user(substitute_user_obj)
+    # Temporary fix: This should change once CKAN github issue 7581 is resolved
+    # https://github.com/ckan/ckan/issues/7581
+    current_app.login_manager._update_request_context_with_user(substitute_user_obj)
+
